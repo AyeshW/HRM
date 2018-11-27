20185dc6835cfb9eeac20414610223219023c979
Charith <rajithacharith@gmail.com>
2018-11-27 21:08:49 +0530

    Add hrm dashboard

---
+++ php/HRMemployeeDetails.php
@@ -0,0 +1,96 @@
+<?php
+session_start();
+if (!$_SESSION['loggedin']){ 
+    header("Location:../index.html");
+    die();
+}
+include '../config/db_connection.php';
+
+
+$conn = OpenCon();
+
+$sql="SELECT * FROM employee_details_hr";
+
+$allData=$conn -> query($sql);
+
+$array = array();
+while($row = mysqli_fetch_assoc($allData)){
+    $array[] = $row;
+}
+
+
+
+CloseCon($conn);
+/* 
+foreach($array as $details){
+    echo $details["First_Name"],$details["Gender"],"<br>";
+} */
+?>
+
+<html>
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <title>DBMS</title>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+</head>
+<body>
+    <div class="container">
+        <br><br>
+        <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">Employee ID</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Gender</th>
+            <th scope="col">Job Role</th>
+            <th scope="col">Department Name</th>
+            <th scope="col">Employee Status</th>
+
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+              foreach($array as $details){  
+            ?>
+            <tr>
+            <th scope="row"><?php echo $details["Employee_id"]; ?></th>
+            <td><?php echo $details["First_Name"]; ?></td>
+            <td><?php echo $details["Last_Name"]; ?></td>
+            <td><?php echo $details["Gender"]; ?></td>
+            <td><?php echo $details["Job_Name"]; ?></td>
+            <td><?php echo $details["Department_Name"]; ?></td>
+            <td><?php echo $details["Status_name"]; ?></td>
+            </tr>
+              <?php } ?>
+        </tbody>
+        </table>
+
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
