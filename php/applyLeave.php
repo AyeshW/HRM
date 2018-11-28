4e584b47c7ede0c5916684dad2493e50e2c398c9
Kalana Wijethunga <kalana.16@cse.mrt.ac.lk>
2018-11-29 04:09:40 +0530

    apply leave changes

--- php/applyLeave.php
+++ php/applyLeave.php
@@ -12,18 +12,15 @@ $dbpass = $_SESSION["dbpass"];
 $conn = OpenCon($dbuser,$dbpass);
 
 $empID=$_POST["empID"];
+
 $sDate=$_POST["sDate"];
+$sDate=date('Y-m-d H:i:s', strtotime($sDate));
 $eDate=$_POST["eDate"];
+$eDate=date('Y-m-d H:i:s', strtotime($eDate));
 $type=$_POST["type"];
 $reason=$_POST["reason"];
 
-echo($dbuser);
-echo($dbpass);
-echo ($empID);
-echo ($sDate);
-echo ($eDate);
-echo ($type);
-echo ($reason);
+
 
 
 
@@ -36,5 +33,7 @@ $allData = $stmt->get_result();
 print_r($allData);
 CloseCon($conn);
 
-
+echo ("leave pending");
 ?>
+
+
