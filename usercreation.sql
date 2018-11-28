707ded2b86ba93de60d1f3d2b65e4bbc04bc6910
Kalana Wijethunga <kalana.16@cse.mrt.ac.lk>
2018-11-29 03:38:52 +0530

    added employee leaves

--- usercreation.sql
+++ usercreation.sql
@@ -1,5 +1,5 @@
 CREATE USER 'universal'@'localhost' IDENTIFIED BY '1234';
-GRANT SELECT ON edbms.user TO 'universal'@'localhost'
+GRANT SELECT ON edbms.user TO 'universal'@'localhost';
 
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin@123';
 CREATE USER 'HRM'@'localhost' IDENTIFIED BY 'HRM@123';
