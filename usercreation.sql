2fa5b5d40f44559df71cb62c807de966fd4b0f64
Charith <rajithacharith@gmail.com>
2018-11-29 01:29:00 +0530

    user creation updated

--- usercreation.sql
+++ usercreation.sql
@@ -4,3 +4,16 @@ GRANT SELECT ON edbms.user TO 'universal'@'localhost'
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin@123';
 CREATE USER 'HRM'@'localhost' IDENTIFIED BY 'HRM@123';
 CREATE USER 'EMP'@'localhost' IDENTIFIED BY 'EMP@123';
+
+GRANT ALL PRIVILEGES ON edbms.address TO 'admin'@'localhost';
+GRANT ALL PRIVILEGES ON edbms.department TO  'admin'@'localhost';
+GRANT ALL PRIVILEGES ON edbms.dependent_info TO  'admin'@'localhost';
+GRANT ALL PRIVILEGES ON edbms.emergency_details TO 'admin'@'localhost';
+GRANT ALL PRIVILEGES ON edbms.employee TO 'admin'@'localhost';
+GRANT ALL PRIVILEGES ON edbms.employee_leaves TO 'admin'@'localhost';
+GRANT ALL PRIVILEGES ON edbms.employementdetails TO 'admin'@'localhost';
+GRANT ALL PRIVILEGES ON edbms.employment_status TO 'admin'@'localhost';
+GRANT ALL PRIVILEGES ON edbms.job_title TO 'admin'@'localhost';
+GRANT ALL PRIVILEGES ON edbms.organization TO 'admin'@'localhost';
+GRANT ALL PRIVILEGES ON edbms.payroll_info TO 'admin'@'localhost';
+GRANT ALL PRIVILEGES ON edbms.pay_grade TO 'admin'@'localhost';
